51ff3eac626c934d08331f1c225252000a0b5b86
Eliran Abdoo <eliran@riverside.fm>
2024-05-22 14:59:47 +0000

    hifigan update

--- hifi_gan_bwe/scripts/export.py
+++ hifi_gan_bwe/scripts/export.py
@@ -32,7 +32,7 @@ def main() -> None:
     parser.add_argument(
         "--log_path",
         type=Path,
-        default="logs",
+        default="/data/home/eliran/trainings/audio-enhancement/hifi-gan-bwe",
         help="training log root path",
     )
 
