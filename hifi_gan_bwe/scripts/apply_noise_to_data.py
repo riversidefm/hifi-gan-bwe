33823ee72248db7d0ac52ec894643560e1f12737
Eliran Abdoo <eliran@riverside.fm>
2024-06-09 15:55:02 +0000

    apply noise script

---
+++ hifi_gan_bwe/scripts/apply_noise_to_data.py
@@ -0,0 +1,82 @@
+import json
+import os
+from pydantic import BaseModel
+import torch
+from tqdm import tqdm
+from hifi_gan_bwe_common.hifi_gan_bwe import datasets
+from hifi_gan_bwe_common.hifi_gan_bwe.scripts.train import (
+    DatasetSplit,
+    DatasetType,
+    load_dataset,
+)
+from scipy.io.wavfile import write as write_wav
+
+
+class Config(BaseModel):
+    sample_rate: int = 48_000
+    seed: int = 1337
+    num_epochs: int = 5
+
+    noise_path: str = "/data/home/eliran/datasets/DNS-Challenge/"
+    dataset_path: str = (
+        "/data/home/eliran/datasets/riverside-audio/vad-wavs/train/manifest.json"
+    )
+    dataset_type: DatasetType = DatasetType.RIVERSIDE
+    dataset_split: DatasetSplit = DatasetSplit.TRAINING
+
+    dataset_tgt_path: str = (
+        "/data/home/projects/audio-enhancement/mp-senet/clean-noisy-pairings/riverside-5epochs"
+    )
+
+
+def main(config: Config):
+    torch.manual_seed(config.seed)
+
+
+    os.makedirs(config.dataset_tgt_path, exist_ok=False)
+    noised_dataset_tgt_path = os.path.join(config.dataset_tgt_path, "noisy")
+    clean_dataset_tgt_path = os.path.join(config.dataset_tgt_path, "clean")
+    for p in [
+        noised_dataset_tgt_path,
+        clean_dataset_tgt_path,
+    ]:
+        os.makedirs(p, exist_ok=False)
+
+    noise_set = datasets.DNSDataset(config.noise_path)
+    data_set = load_dataset(
+        dataset_type=config.dataset_type,
+        dataset_split=config.dataset_split,
+        path=config.dataset_path,
+    )
+    loader = torch.utils.data.DataLoader(
+        data_set,
+        collate_fn=datasets.Preprocessor(
+            noise_set=noise_set, training=True, return_original_audio=True
+        ),
+        batch_size=datasets.BATCH_SIZE,
+        shuffle=False,
+        drop_last=True,
+    )
+    batch_idx = 0
+    for epoch in tqdm(range(config.num_epochs)):
+        for batch in loader:
+            _, _, audio_tensor_noisy, audio_tensor_clean = batch
+
+            for audio_tensor, dst_dir in [
+                (audio_tensor_noisy, noised_dataset_tgt_path),
+                (audio_tensor_clean, clean_dataset_tgt_path),
+            ]:
+                for j, audio_sample in enumerate(audio_tensor):
+                    audio_sample = audio_sample.squeeze().detach().cpu().numpy()
+                    filename = f"{epoch}_{batch_idx}_{j}.wav"
+                    dst_path = os.path.join(dst_dir, filename)
+                    write_wav(dst_path, config.sample_rate, audio_sample)
+            batch_idx += 1
+
+    config_dict = config.dict()
+    with open(os.path.join(config.dataset_tgt_path, "config.json"), "w") as f:
+        f.write(json.dumps(config_dict))
+
+
+if __name__ == "__main__":
+    main(config=Config())
